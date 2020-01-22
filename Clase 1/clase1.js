7b145c0d304c7d3aadb72ee3dfe65ebecfadf274
Vandersal Aheram <163239@ids.upchiapas.edu.mx>
2020-01-22 01:02:37 -0600

    Codigo de la clase 1

---
+++ Clase 1/clase1.js
@@ -0,0 +1,9 @@
+
+var nombre = 'Aheram' , apellido = 'Archila'
+var nombre1
+var edad = 21
+
+nombre1 = 'Vandersal'
+
+console.log('Hola ' + nombre + ' ' + apellido + ' y tienes ' + edad  + ' años')
+
